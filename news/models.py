6381abe2b8fc89a63443b2a492a944b629a71585
byoungjupark <byoungju1012@gmail.com>
2021-10-13 22:44:31 +0900

    api_view based on function

--- news/models.py
+++ news/models.py
@@ -12,4 +12,4 @@ class Article(models.Model):
     updated_at       = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"{self.author } {self.title }"
+        return f"{self.author} {self.title}"
